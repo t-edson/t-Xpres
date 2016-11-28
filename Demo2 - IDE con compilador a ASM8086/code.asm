993b3de837e0e8a658015b55ccfcb29884bc89d9
Tito Hinostroza <dev_null_1@hotmail.com>
2016-11-27 20:53:55 -0500

    Add files via upload

--- Demo2 - IDE con compilador a ASM8086/code.asm
+++ Demo2 - IDE con compilador a ASM8086/code.asm
@@ -1,13 +1,19 @@
-
 .MODEL TINY
 .DATA
   _tmpStr0 DB 255 DUP('#'),0
   x DW 0
 .CODE
   ;expres
-  mov x, 3
+  mov ax, 1
+  add ax, x
+  mov x, ax
   ;fin expres
   ;expres
-  mov ax,5
-  mov ax, 3
+  mov al, '5'
+  mov al, ''
   push ax
+  pop dx
+  mov ah, 02h
+  int 21h
+  ;fin expres
+END
